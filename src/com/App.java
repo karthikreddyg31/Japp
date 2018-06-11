945fccf9bb6da1aa62293eea0789672913908593
karthik1 <user2@accenture.com>
2018-06-11 14:38:30 +0530

    sop updated

--- src/com/App.java
+++ src/com/App.java
@@ -6,6 +6,6 @@ public class App {
 		
 		System.out.println("Hello World");
 		System.out.println("Hello World Karthik");
-		System.out.println("Hello World Accenture");
+		System.out.println("Hello World Accenture HDC3");
 	}
 }
